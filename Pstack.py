7f9b6aaed7653fa1bc3f632f922edd73a1869a54
prathameshap <prathameshpawar1301@gmail.com>
2020-04-27 19:34:58 -0700

    Master Stack class

---
+++ Pstack.py
@@ -0,0 +1,35 @@
+#!/etc/bin/env python3
+"""Filename: Pstack"""
+
+#Auther Name: Prathamesh Pawar
+#       Email: prathameshpawar1301@gmail.com
+
+"""This is Stack class"""
+
+class Pstack:
+
+    __init__(self):
+        self.items = []
+
+    def push(self.item):
+
+        self.items.append(item)
+
+    def pop(self):
+
+        if self.items:
+            return self.items.pop(item)
+
+
+    def peek(self):
+
+        return self.items[-1]
+
+
+    def size(self):
+
+        return len(self.items)
+
+    def isEmpty(self):
+
+        return self.items = []
