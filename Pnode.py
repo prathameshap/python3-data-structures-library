f73fe2b3b2b437245d53caecd94a600b162a4309
prathameshap <prathameshpawar1301@gmail.com>
2020-04-27 17:31:54 -0700

    Master- Node Class

---
+++ Pnode.py
@@ -0,0 +1,15 @@
+#!/ect/bin/env python3
+
+"""ClassName :: Pnode"""
+
+#Auther Name: Prathamesh Pawar 
+#       Email: prathameshpawar1301@gmail.com
+
+"""This Class is created as part of User drived data structes 
+Namely: Linked List, Stack, Queue and Dequeue"""
+
+class Pnode:
+
+    def __init__(self, dataval = None):
+        self.dataval = dataval
+        self.nextval = None
