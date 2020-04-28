c0ef0bf81ef321de415ce142ee0980113b6e93fa
prathameshap <prathameshpawar1301@gmail.com>
2020-04-28 11:29:16 -0700

    Queue class with working functionality without testing

---
+++ Pqueue.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""This is queue data structure file"""
+
+#Auther Name: Prathamesh Pawar
+#       Email: prathameshpawar1301@gmail.com
+
+class Queue:
+
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        if self.items:
+            return self.items.pop(item)
+        return None
+
+    def peek(self):
+        if self.items:
+            return self.items[-1]
+        return None
+
+    def size(self):
+        return len(self.items)
+
+    def isEmpty(self):
+        return self.items == []
