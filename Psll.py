f350075bb8721fbb516e6c484b0fc31b91bf8532
prathameshap <prathameshpawar1301@gmail.com>
2020-04-29 14:05:35 -0700

    Started editing singly linked list class

---
+++ Psll.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""This file includes sinle linked list class"""
+
+#Author Name: Prathamesh Pawar
+#       Email: prathameshpawar1301@gmail.com
+
+class SLL:
+
+    def __init__(self):
+        self.head = None
+
+    def __repr__(self):
+        return "Node obbject: data = {}".format(self.data)
+
+
